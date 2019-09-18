b4d29587e16e92195e1a0005626de13aa2c9e8ea
Sangjin Lee <leesj2004@gmail.com>
2019-09-18 15:02:48 +0900

    2019-09-18 update

--- ff.bat
+++ ff.bat
@@ -1,16 +1,16 @@
 @ECHO OFF
 ::
 :: 2019-06-26
+:: 2019-09-18
 ::
 
 SETLOCAL
 
 IF "%1" == "" 	GOTO :enter
 
-:: there is a parameter.
+:: there is a argument.
 ::
 SET name="%1*"
-
 ECHO.
 DIR /s /b %name% 2> NUL | FIND "" /v /c
 ECHO.
@@ -20,13 +20,22 @@ GOTO :EOF
 
 
 :enter
+ECHO.
 SET /P n1="Enter the name: "
 SET name="%n1%*"
 
+IF %name% NEQ "*"		GOTO :search
+SET mmdd=%date:~5,2%%date:~-2%
+SET name="%mmdd%*"
+ECHO %mmdd%
+
+
+
+:search
 ECHO.
 DIR /s /b %name% 2> NUL | FIND "" /v /c
 ECHO.
 
+
 PAUSE
 GOTO :EOF
-
