0e21bb69bf847ae5f69a4db71ba3c8b106115aee
Sangjin Lee <leesj2004@gmail.com>
2019-12-25 16:27:43 +0900

    webhelper remove

---
+++ mytorrent.bat
@@ -0,0 +1,43 @@
+@ECHO OFF
+::
+:: 2019-12-25
+::
+
+SETLOCAL
+
+set TPATH=C:\Users\%USERNAME%\AppData\Roaming\uTorrent\updates
+set TFILE=utorrentie.exe
+
+for /f %%A in ('dir /b /a:d /o:-n %TPATH%') do (set TDIR=%%A&& goto :first)
+
+
+:first
+if not exist %TPATH%\%TDIR%\%TFILE% (
+	echo not found
+	exit /b
+)
+
+:: rename command cannot assign target directory
+::
+if exist %TPATH%\%TDIR%\%TFILE% (
+	if not exist %TPATH%\%TDIR%\_%TFILE% (
+		ren %TPATH%\%TDIR%\%TFILE% _%TFILE%
+
+		echo renamed %TPATH%\%TDIR%\_%TFILE% 
+		echo.
+	) else (
+		echo existed %TPATH%\%TDIR%\_%TFILE%
+		echo.
+	)
+)
+
+:: create empty file
+::
+if not exist %TPATH%\%TDIR%\%TFILE% (
+	type NUL > %TPATH%\%TDIR%\%TFILE%
+
+	echo created %TPATH%\%TDIR%\%TFILE%
+	echo.
+)
+
+exit /b
